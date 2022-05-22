169d544bb4f736e3a14be0f8d4b5523b83b18c5b
Patrycja <filipekpatrycjaa@gmail.com>
2022-05-22 11:29:02 +0200

    Inicjalizacja projektu

---
+++ src/main/java/pl/wszib/pizzamarketproject/PizzamarketprojectApplication.java
@@ -0,0 +1,13 @@
+package pl.wszib.pizzamarketproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzamarketprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzamarketprojectApplication.class, args);
+	}
+
+}
